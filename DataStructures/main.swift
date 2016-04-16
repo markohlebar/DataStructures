f4c0d63b68003f45c8dab403eb496a5220638a84
Marko Hlebar <marko.hlebar@gmail.com>
2016-04-16 10:48:07 +0100

    Initial Commit

---
+++ DataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataStructures
+//
+//  Created by Marko Hlebar on 16/04/2016.
+//  Copyright © 2016 Marko Hlebar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
